41db9c1f7c95e5ae30265bab9ea33ed661798ad3
soham009 <sohamthaker17@gmail.com>
2019-01-02 17:35:09 +0000

    Done

--- q03_remove_wordcount/build.py
+++ q03_remove_wordcount/build.py
@@ -1,7 +1,12 @@
+# %load q03_remove_wordcount/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from greyatomlib.episource_python_guided_project.q02_create_dataframe.build import q02_create_dataframe
 path = 'data/episource.txt'
 
+def q03_remove_wordcount(path):
+    df =q02_create_dataframe(path)
+    data = df[df['count'] >= 5]
+    return data
 
-def q03_remove_wordcount():
+
